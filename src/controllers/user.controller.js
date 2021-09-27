cc65c5de66f5f755e1f9b6e4916078108c3be269
Sudhir <82999615+devSudhir@users.noreply.github.com>
2021-09-27 17:31:28 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,10 +13,11 @@ router.post(
   authorise(["admin"]),
   upload.single("profile_photo"),
   async (req, res) => {
-    const errors = validationResult(req);
+  /*  const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    */
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
@@ -29,12 +30,12 @@ router.post(
 );
 
 router.get("/", async (req, res) => {
-  if (!req.body.token) {
+ /* if (!req.body.token) {
     return res
       .status(400)
       .json({ status: "failed", message: "User is not logged in" });
   }
-
+*/
   const user = await User.find().lean().exec();
   return res.status(200).json({ user });
 });
